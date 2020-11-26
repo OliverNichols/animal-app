4167ea4e28750cb93eda3498d8423e4f7d0ee2fc
Ubuntu <azureuser@jenkins.j2hh0flix4se5f1k41rjua0dsh.zx.internal.cloudapp.net>
2020-11-26 16:33:40 +0000

    Added testing script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
 
+docker build -t testing-img -f testing/Dockerfile .
+docker run -it -d --name testing-cont testing-img
+
+docker exec testing-cont pytest ./service-1 --cov ./service-1
+docker exec testing-cont pytest ./service-2 --cov ./service-2
+
+docker stop testing-cont
+docker rm testing-cont
