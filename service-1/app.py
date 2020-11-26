d7d99820455f300aa3a64d5a8fa3aa534df2ed9e
BenHesketh21 <heskethben0@gmail.com>
2020-11-26 12:18:52 +0000

    Added repo

---
+++ service-1/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import requests
+
+app =  Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Gets an animal
+    animal = requests.get("http://service-2:5001/animal")
+    # Gets the noise
+    noise = requests.post("http://service-2:5001/noise", data=animal.text)
+
+    return render_template('index.html', animal=animal.text, noise=noise.text)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
